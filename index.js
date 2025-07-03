9c311d04e7968d6f3be5a5ecbb4e938954c9f811
kostyantynvernik <kostyantynvernik@gmail.com>
2025-07-03 12:50:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,9 +46,12 @@ color: #FAF8F0;
 
 text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.004);
 }
-.best-sellers-container{padding: 0 30px!important;max-height: 750px!important;}
+//.swiper-slide-next {visibility:hidden!important}
+.best-sellers-wrapper {display: flex !important;}
+.cta--custom-wrapper {margin-top: 0!important;}
+.best-sellers-container{padding: 0 30px!important;max-height: 100%!important;}
 .best-sellers-wrapper {width:100%;max-height: 700px!important;}
-.best-sellers-item-top {display: table-cell !important;width: 100%;}
+.best-sellers-item-top {display: table-cell !important;width: 100%;padding-bottom: 5px;}
 .newmainbloacktopchild5{font-family: 'Poppins';
 font-style: normal;
 font-weight: 800;
